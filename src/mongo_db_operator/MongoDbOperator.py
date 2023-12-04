0c5956ee7e4139a551fb6aa85d13eff4908ccf51
Tesla2000 <fratajczak124@gmail.com>
2023-12-04 14:50:24 +0100

    Added load_multiple method

--- src/mongo_db_operator/MongoDbOperator.py
+++ src/mongo_db_operator/MongoDbOperator.py
@@ -1,4 +1,5 @@
-from typing import Type, Iterable, TypeVar
+from threading import Thread
+from typing import Type, Iterable, TypeVar, Any, Sequence
 
 from pymongo.database import Database
 from seriattrs import DbClass
@@ -15,10 +16,19 @@ class MongoDbOperator:
     def delete(self, element: DbClass):
         self.known_classes[type(element)].delete(element)
 
-    def load(self, element_class: T, element_id: str) -> T:
+    def load(self, element_class: T, element_id: Any) -> T:
         return self.known_classes[element_class].load(element_id)
 
-    def load_or_default(self, element_class: T, element_id: str, default=None) -> T:
+    def load_multiple(self, element_class: T, element_ids: Sequence[Any]) -> list[T]:
+        results = [T for _ in element_ids]
+        threads = tuple(
+            Thread(target=lambda index, element_id: results.__setitem__(
+                index, self.known_classes[element_class].load(element_id)),
+                   args=(index, element_id)).start() for index, element_id in enumerate(element_ids))
+        tuple(map(Thread.join, threads))
+        return results
+
+    def load_or_default(self, element_class: T, element_id: Any, default=None) -> T:
         try:
             return self.load(element_class, element_id)
         except NoSuchElementException:
